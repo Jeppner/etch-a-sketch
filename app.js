c5d926741e5c97ddc9ad2fa8f0a37e532697c258
Jess <jessicadixie14@hotmail.com>
2022-12-15 18:35:11 -0800

    Added styling changes and eraser functionality

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ let currentNumSquares = document.querySelector('.currentNumSquares');
 let numSquares = parseInt(document.querySelector('#numSquares').value);
 let squares = document.querySelectorAll('.square');
 
+let tools = Array.from(document.querySelectorAll('.tool'));
+let penColour = document.querySelector('#drawingColour');
+let eraser = document.querySelector('#eraser');
+
+let reset = document.querySelector('#reset');
+
 function updateSquares () {
-    currentNumSquares.textContent = document.querySelector('#numSquares').value;
+    currentNumSquares.textContent = `${document.querySelector('#numSquares').value} x ${document.querySelector('#numSquares').value}`;
     clearBoard();
-    createBoard();
 }
 
 function clearBoard() {
     while(board.firstChild) {
         board.removeChild(board.lastChild);
     }
+    backgroundColour.value = '#FFFFFF';
+    board.style.backgroundColor = '#FFFFFF';
+    drawingColour.value = '#000000';
+    createBoard();
 }
 
 function createBoard () {
@@ -52,3 +61,27 @@ function chooseBackground () {
     board.style.backgroundColor = backgroundColour;
 }
 
+
+function erase () {
+    squares = document.querySelectorAll('.square');
+    squares.forEach(function(elem) {
+        elem.addEventListener("mousemove", function(e) {
+            if(e.buttons == 1 ) {
+                e.preventDefault();
+            elem.style.backgroundColor = 'transparent';
+            }
+        });
+    });
+}
+
+
+const activeBtn = (e) => {
+  tools.forEach(node => {
+    node.classList.remove('active');
+  });
+  e.currentTarget.classList.add('active');
+}
+
+tools.forEach(node => {
+  node.addEventListener('click', activeBtn)
+});
